0243b36ba4240dbcab4bb4d28c752e8d15c47207
Junior-Stranner <JuniorStranner@gmail.com>
2024-05-09 17:31:53 -0300

    completlyng the method and testing until the final method delete

--- bibliotec/src/main/java/br/com/judev/bibliotec/service/serviceImpl/AuthorServiceImpl.java
+++ bibliotec/src/main/java/br/com/judev/bibliotec/service/serviceImpl/AuthorServiceImpl.java
@@ -3,15 +3,21 @@ package br.com.judev.bibliotec.service.serviceImpl;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import br.com.judev.bibliotec.dtos.mapper.AuthorMapper;
+import br.com.judev.bibliotec.dtos.mapper.CategoryMapper;
 import br.com.judev.bibliotec.dtos.requestDto.AuthorRequestDto;
 import br.com.judev.bibliotec.dtos.responseDto.AuthorResponseDto;
 import br.com.judev.bibliotec.entity.Author;
+import br.com.judev.bibliotec.entity.Category;
+import br.com.judev.bibliotec.entity.City;
+import br.com.judev.bibliotec.entity.Zipcode;
 import br.com.judev.bibliotec.repository.AuthorRepository;
 import br.com.judev.bibliotec.service.AuthorService;
 import br.com.judev.bibliotec.service.ZipcodeService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 
 @Service
@@ -31,7 +37,7 @@ public class AuthorServiceImpl implements AuthorService{
 
          Author author = new Author();
         author.setName(authorRequestDto.getName());
-        
+
         authorRepository.save(author);
         return AuthorMapper.ToDto(author);
     }
@@ -42,28 +48,55 @@ public class AuthorServiceImpl implements AuthorService{
     }
     @Override
     public AuthorResponseDto getAuthorById(Long authorId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAuthorById'");
+       Author author = authorRepository.findById(authorId).orElseThrow(() ->
+                new IllegalArgumentException("could not find Author with id: " + authorId));
+        return AuthorMapper.ToDto(author);
     }
     @Override
     public Author getAuthor(Long authorId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAuthor'");
+       return authorRepository.findById(authorId).orElseThrow(() ->
+                new IllegalArgumentException("could not find Author with id: " + authorId));
     }
     @Override
     public AuthorResponseDto deleteAuthor(Long authorId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteAuthor'");
+         Author author = getAuthor(authorId);
+        authorRepository.delete(author);
+        return AuthorMapper.ToDto(author);
     }
+    
     @Override
     public AuthorResponseDto editAuthor(Long authorId, AuthorRequestDto authorRequestDto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'editAuthor'");
+        if(authorRequestDto == null || authorRequestDto.getName() == null || authorRequestDto.getName().isBlank()){
+            System.out.println("Author name is null or blank.");
+            throw new IllegalArgumentException("Author name cannot be null or blank.");
+        }
+
+        Author author = new Author();
+        author.setName(authorRequestDto.getName());
+
+        authorRepository.save(author);
+        return AuthorMapper.ToDto(author);
     }
     @Override
     public AuthorResponseDto addZipcodeToAuthor(Long authorId, Long zipcodeId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addZipcodeToAuthor'");
+      Author author = getAuthor(zipcodeId);
+        if (author == null) {
+            throw new EntityNotFoundException("Zipcode with ID " + zipcodeId + " not found.");
+        }
+    
+        Zipcode zipcode = zipcodeService.getZipcode(zipcodeId);
+        if (zipcode == null) {
+            throw new EntityNotFoundException("City with ID " + zipcodeId + " not found.");
+        }
+    
+        if (zipcode.getCity() != null) {
+            throw new IllegalArgumentException("Zipcode already has a city.");
+        }
+    
+        author.setZipcode(zipcode);
+        authorRepository.save(author);
+
+        return AuthorMapper.ToDto(author);
     }
     @Override
     public AuthorResponseDto deleteZipcodeFromAuthor(Long authorId) {
@@ -71,6 +104,5 @@ public class AuthorServiceImpl implements AuthorService{
         throw new UnsupportedOperationException("Unimplemented method 'deleteZipcodeFromAuthor'");
     }
 
-    
     
 }
